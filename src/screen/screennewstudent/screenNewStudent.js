92b60e3b488616b12957a06374ab175ab1d48cfa
Mouni19 <mesbahmouni@gmail.com>
2022-02-07 20:03:35 +0100

    ca marche

---
+++ src/screen/screennewstudent/screenNewStudent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NewStudent from 'components/ajoutnouvetudiant/newStudent';
+
+
+
+const Login = () =>{
+        return(
+            <div className='newStudent_container'>
+        
+                <NewStudent/>
+        
+    </div>
+        );
+    }
+
+
+export default Login;
